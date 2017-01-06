6112a6c0f5a73e0517d39862e053c829b2ab379a
Waleed El-Geresy <noEmail>
2017-01-06 02:39:04 +0000

    Created a supposedly working MultImproved, but it needs testing!

--- ieeeMult.s
+++ ieeeMult.s
@@ -1,5 +1,8 @@
-			MOV		R0,#0x40000000
-			MOV		R1,#0x00000001
+			MOV		R0,#0xFF
+			LSL		R0,R0,#1
+			ADD		R0,R0,#1
+			LSL		R0,R0,#23
+			MOV		R1,#1
 			BL		IEEE754MULT
 			END
 			
@@ -10,48 +13,67 @@ IEEE754MULT
 			MOV		R5,#0xFFFFFF
 			LSR		R5,R5,#1 ;AND MASK FOR MANTISSA
 			MOV		R8,#1
+			;STORE	EXPONENTS
+			AND		R7,R4,R1, LSR #23
+			AND		R6,R4,R0, LSR #23
 			
-			;R10		will store the sign
+			;R10		and R11will store the sign
 			MOV		R10,R0,LSR #31
-			EOR		R10,R10,R1, LSR #31 ;contains sign bit inLSB position
+			MOV		R11,R1, LSR #31 ;contains sign bit in LSB position
 			
 			;MULTIPLICATION	into R2-R3
-			STMED	R13!,{R0,R1}
 			LSL		R0,R0,#9
 			LSR		R0,R0,#9
-			ADD		R0,R0,R8, LSL #23 ;add in a 1 for pre-multiplication preparation
 			LSL		R1,R1,#9
 			LSR		R1,R1,#9
-			ADD		R1,R1,R8, LSL #23
+			
+			;CHECK	IF THEY ARE NOT A NUMBER OR INFINITY
+			CMP		R7,#255 ;Max threshold - we do the second one (R7) first because it takes presedence according to the testbench (not a number case)
+			BNE		AFTER
+			CMP		R1,#0
+			MOVEQ	R2,R1
+			ADDEQ	R2,R2,R7, LSL #23
+			ADDEQ	R2,R2,R11,ROR #1
+			BEQ		NOTANUMBER
+			
+AFTER		CMP		R6,#255 ;Max threshold
+			BNE		AFTER2
+			CMP		R0,#0
+			MOVEQ	R2,R0
+			ADDEQ	R2,R2,R6, LSL #23
+			ADDEQ	R2,R2,R10,ROR #1
+			BEQ		NOTANUMBER
+			
+			;HANDLING DENORMAL AND ZERO
+AFTER2		CMP		R6,#0 ;compare exponent to zero
+			ADDNE	R0,R0,R8, LSL #23 ;add in a 1 for pre-multiplication preparation only if the mantissa is not 0
+			CMP		R7,#0
+			ADDNE	R1,R1,R8, LSL #23
+			
 			BL		MUL24X24 ;multiply R0 and R1, store in R2-3
-			LDMED	R13!,{R0,R1} ;Keep R0 and R1
+			
+			;R7		will store the summed exponent
+			SUB		R7,R7,#127 ;equivalent to taking away 127 for each and then adding 127
+			ADD		R7,R6,R7	;R6 stores the result
 			
 			;ADD		ON ADDITIONAL EXPONENT FOR THRESHOLD stored in R9 by subroutine and also produces the mantissa in R2
 			BL		ADDITEXPON
 			AND		R2,R2,R5 ;take only the first 23 bits (cut off the 1 in the 24th bit (2^23))
 			ADD		R7,R7,R9 ;add on any necessary extra to R7
 			
-			;R7		will store the summed exponent
-			AND		R7,R4,R1, LSR #23
-			CMP		R6,#255 ;Max threshold - we do the second one first because it takes presedence according to the testbench
-			MOVGT	R2,R1
-			BGT		NOTANUMBER
-			AND		R6,R4,R0, LSR #23
-			CMP		R6,#255 ;Max threshold
-			MOVGT	R2,R0
-			BGT		NOTANUMBER
-			SUB		R7,R7,#127 ;equivalent to taking away 127 for each and then adding 127
-			ADD		R7,R6,R7	;R6 stores the result
-			
 			;THRESHOLDS
-			CMP		R7,#0 ;min threshold
-			MOVLT	R7,#0 ;make the mantissa as small as possible
-			MOVLT	R2,#0 ;make the significand/mantissa zero (as small as possible)
+			;If		it is under zero then we should continually add to the exponent while shifting the mantissa right until it is equal to 0
+			HANDLE THE CASE WHEN IT IS DENORMAL USING ABOVE
+;			CMP		R7,#0 ;min threshold
+;			MOVLE	R7,#0 ;make the mantissa as small as possible
+;			MOVLE	R2,#0 ;make the significand/mantissa zero (as small as possible)
 			CMP		R7,#255 ;Max threshold
-			MOVGT	R7,#255
+			MOVGE	R7,#255
+			MOVGE	R2,#0 ;infinity has 0 mantissa
 			
 			;BRING	IT ALL TOGETHER
 			ADD		R2,R2,R7, LSL #23 ;23 -> smallest one is in the 24th bit
+			EOR		R10,R10,R11 ;we form the sign bit lastly
 			ADD		R2,R2,R10, LSL #31 ;was in the LSB, now in the MSB
 			
 NOTANUMBER	LDMED	R13!,{R0,R1,R3-R12,PC}
